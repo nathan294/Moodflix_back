5ccaafd2df58b34d87c2d9516536c73c2d46e25e
Nathan <nathanboulogne@gmail.com>
2023-09-28 22:14:03 +0000

    Modif in init.sh

--- .dev/init.sh
+++ .dev/init.sh
@@ -3,8 +3,6 @@
 cd /app
 
 poetry install --with dev
-# git init
-# git config --global --add safe.directory /app/workspace
 poetry run pre-commit install -f --hook-type pre-commit
 poetry run pre-commit autoupdate
 
