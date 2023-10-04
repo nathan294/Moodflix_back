b0f408c2de30542e96bdf0a537850fa04e0ae271
Nathan <nathanboulogne@gmail.com>
2023-10-04 18:55:44 +0000

    Endpoints listes de films (populaires etc..)

--- api/movie/data_processing.py
+++ api/movie/data_processing.py
@@ -1,5 +1,7 @@
 from typing import Dict, List
 
+import api.movie.schemas as sch
+
 
 def concatenate_genres(movie_genres: List[Dict], tv_genres: List[Dict]) -> List[Dict]:
     """
@@ -11,3 +13,11 @@ def concatenate_genres(movie_genres: List[Dict], tv_genres: List[Dict]) -> List[
         genre_name = genre["name"]
         concatenated[genre_id] = genre_name
     return [{"id": k, "name": v} for k, v in concatenated.items()]
+
+
+def concatenate_home_lists(
+    popular_movies: List[sch.MovieCreate],
+    now_playing_movies: List[sch.MovieCreate],
+    upcoming_movies: List[sch.MovieCreate],
+):
+    return {"popular": popular_movies, "now_playing": now_playing_movies, "upcoming": upcoming_movies}
--- api/movie/router.py
+++ api/movie/router.py
@@ -5,10 +5,10 @@ from sqlalchemy.orm import Session
 
 import api.movie.schemas as sch
 from api.commons.dependencies import get_db
-from api.movie.data_processing import concatenate_genres
+from api.movie.data_processing import concatenate_genres, concatenate_home_lists
 from api.movie.get_database import get_genre_names_from_database
 from api.movie.post_database import insert_genres_in_database, insert_movies_in_database
-from api.movie.tmdb_api import get_genres_from_tmdb, search_movie_in_tmdb_api
+from api.movie.tmdb_api import get_genres_from_tmdb, get_list_from_tmdb, search_movie_in_tmdb_api
 
 router = APIRouter(
     prefix="/movie",
@@ -50,3 +50,30 @@ async def get_genre_names(genre_ids: sch.GenreIds, db: Session = Depends(get_db)
     Retrieve movie genre names from database
     """
     return get_genre_names_from_database(genre_ids=genre_ids, db=db)
+
+
+@router.get("/movies_list", response_model=List[sch.MovieCreate])
+async def get_movies_list(wanted_list: sch.WantedList):
+    """
+    Get list from TMDB API
+    * Now Playing : To get movies currently in theaters, use _now\_playing_
+    * Popular : To get popular movies, use _popular_
+    * Top Rated : To get top rated movies, use _top\_rated_
+    * Upcoming : To get upcoming movies, use _upcoming_
+    """
+    return get_list_from_tmdb(wanted_list)
+
+
+@router.get("/home_page_lists", response_model=sch.HomePageMovies)
+async def get_home_page_movies_lists():
+    """
+    Get all data for the home screen page. Basically, it retrieve and concatenate:
+    * Now Playing movies
+    * Popular movies
+    * Upcoming movies
+    """
+    popular_movies = get_list_from_tmdb(wanted_list=sch.WantedList.popular)
+    now_playing_movies = get_list_from_tmdb(sch.WantedList.now_playing)
+    upcoming_movies = get_list_from_tmdb(sch.WantedList.upcoming)
+    global_list = concatenate_home_lists(popular_movies, now_playing_movies, upcoming_movies)
+    return global_list
