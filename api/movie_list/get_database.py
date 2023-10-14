0b2c321cdfa6698ea179e547db63e69967e6056a
Nathan <nathanboulogne@gmail.com>
2023-10-14 23:11:50 +0000

    MovieList fetch for a user

---
+++ api/movie_list/get_database.py
@@ -0,0 +1,16 @@
+from typing import List
+
+from sqlalchemy import case, desc, select
+from sqlalchemy.orm import Session
+
+from api.models.movie_list import MovieList
+
+
+def get_movie_lists_for_user(user_id: str, db: Session) -> List[MovieList]:
+    stmt = (
+        select(MovieList)
+        .filter_by(user_id=user_id)
+        .order_by(case([(MovieList.locked == True, 1)], else_=0).desc(), desc(MovieList.created_at))
+    )
+    movie_lists = db.execute(stmt).scalars().all()
+    return movie_lists
--- api/movie_list/router.py
+++ api/movie_list/router.py
@@ -1,8 +1,11 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 import api.movie_list.schemas as sch
 from api.commons.dependencies import get_db, verify_firebase_token
+from api.movie_list.get_database import get_movie_lists_for_user
 from api.movie_list.post_database import insert_movie_list_in_database
 
 router = APIRouter(
@@ -14,7 +17,15 @@ router = APIRouter(
 @router.post("/", response_model=sch.MovieList)
 async def create_movie_list(title: str, user_id: str = Depends(verify_firebase_token), db: Session = Depends(get_db)):
     """
-    Create a movie list from title
+    Create a movie list from title for a specific user
     """
 
     return insert_movie_list_in_database(title, user_id, db)
+
+
+@router.get("/", response_model=List[sch.MovieList])
+async def list_movie_lists(user_id: str = Depends(verify_firebase_token), db: Session = Depends(get_db)):
+    """
+    Get all movie lists for a specific user
+    """
+    return get_movie_lists_for_user(user_id, db)
