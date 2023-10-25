cdd9e146fb0ecf3c8073d76314c729968bcbeeb8
Nathan <nathanboulogne@gmail.com>
2023-10-25 13:35:30 +0000

    Récupération des notes

--- api/v1/features/user_interaction/router.py
+++ api/v1/features/user_interaction/router.py
@@ -22,7 +22,7 @@ router = APIRouter(
 ############
 
 
-@router.get("/rate", response_model=List[sch.Rating])
+@router.get("/rate", response_model=List[sch.RatedMovie])
 async def get_user_ratings(
     user_id: str = Depends(verify_firebase_token), db: Session = Depends(get_db), skip: int = 0, limit: int = 10
 ):
